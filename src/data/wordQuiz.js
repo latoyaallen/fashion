6ff593f45ec5459e6bf945a6f3abec3a29bf6b01
latoyaallen <me@tailorlabs.io>
2019-10-23 23:20:03 -0400

    Add word quiz data

---
+++ src/data/wordQuiz.js
@@ -0,0 +1,32 @@
+const lacuna = "an unfilled space or interval; a gap."
+const petrichor = "The pleasant, earthy smell after rain."
+const supine = "Lying face upwards."
+const hiraeth = "missing a time, an era, or a person - including homesickness for what may not exist any longer."
+const wordQuiz = [
+  {
+    "id": "1",
+    "word":"Lacuna",
+    "correctAnswer":lacuna,
+    "possibleAnswers":[hiraeth, petrichor, lacuna, supine],
+  },
+  {
+    "id": "2",
+    "word":"Petrichor",
+    "correctAnswer": petrichor,
+    "possibleAnswers":[hiraeth, petrichor, lacuna, supine],
+  },
+  {
+    "id": "3",
+    "word":"Supine",
+    "correctAnswer":supine,
+    "possibleAnswers":[hiraeth, petrichor, lacuna, supine],
+  },
+  {
+    "id": "4",
+    "word":"Hiraeth",
+    "correctAnswer":hiraeth,
+    "possibleAnswers":[hiraeth, petrichor, lacuna, supine],
+  },
+]
+
+export default wordQuiz;
