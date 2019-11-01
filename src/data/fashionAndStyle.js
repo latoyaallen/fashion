d02ae68a48f56d1240c34377f36f3a0991493ba5
latoyaallen <me@tailorlabs.io>
2019-11-01 18:58:46 -0400

    Add fashionAndStyle.js

---
+++ src/data/fashionAndStyle.js
@@ -0,0 +1,25 @@
+const fashionAndStyle = [
+  {
+    "id": "100",
+    "channel":"Fashion and Style",
+    "creator":"@teaInParis",
+    "title":"My love for New York style captured in one vlog:  WHAT EVERYONE IS WEARING IN NEW YORK | FALL TRENDS 2019 | KarenBritChick",
+    "src":"https://www.youtube.com/embed/IUP1Lp9WQzw",
+  },
+  {
+    "id": "101",
+    "channel":"Fashion and Style",
+    "creator":"@teaInParis",
+    "title":"I adore this dreamy Chanel Spring Summer 2020 collection.",
+    "src":"https://www.youtube.com/embed/BGrY85i-skk",
+  },
+  {
+    "id": "102",
+    "channel":"Fashion and Style",
+    "creator":"@tiaraNYC",
+    "title":"Autumn Lookbook || Mossonyi",
+    "src":"https://www.youtube.com/embed/_6-3W4kxOoo",
+  },
+]
+
+export default fashionAndStyle;
