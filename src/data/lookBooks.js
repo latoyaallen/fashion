dab7e8a7fc224b21ff47f949381ff41ed81c45cf
latoyaallen <me@tailorlabs.io>
2019-11-03 22:40:13 -0500

    Update data

--- src/data/lookBooks.js
+++ src/data/lookBooks.js
@@ -1,35 +1,34 @@
 const lookBooks = [
   {
     "id": "200",
+    "creator":"Samio",
+    "title":"4 Super Simple Everyday Outfits Lookbook | Samio",
+    "src":"https://www.youtube.com/embed/RTmqYADBi1E",
+  },
+  {
+    "id": "201",
     "creator":"Mossonyi",
     "title":"7 Recent Looks | Autumn Lookbook",
     "src":"https://www.youtube.com/embed/_6-3W4kxOoo",
   },
   {
-    "id": "201",
+    "id": "202",
     "creator":"TheNotoriousKIA ",
     "title":"TIPS! How to Create an Outfit WINTER LOOKBOOK-LAYERING 101",
     "src":"https://www.youtube.com/embed/6c2SLsbD1Yw",
   },
   {
-    "id": "202",
+    "id": "203",
     "creator":"HouseOfAesthetics",
     "title":"Autumn Lookbook Transitioning from Summer to Autumn | 2019",
     "src":"https://www.youtube.com/embed/icKCjqBHTDs",
   },
   {
-    "id": "203",
+    "id": "204",
     "creator":"The Yusufs",
     "title":"8 FALL OUTFIT IDEAS | STYLISH FALL LOOKS | THE YUSUFS",
     "src":"https://www.youtube.com/embed/rQa0bIlBSNQ",
   },
-  {
-    "id": "204",
-    "creator":"Samio",
-    "title":"4 Super Simple Everyday Outfits Lookbook | Samio",
-    "src":"https://www.youtube.com/embed/RTmqYADBi1E",
-  },
-
 ]
 
 export default lookBooks;
