a245e8a6c1f2eddccc66dcd71b8ac618d272d363
latoyaallen <me@tailorlabs.io>
2019-11-07 19:30:15 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Body from './components/Body'
+import Footer from './components/Footer'
 
 class App extends React.Component {
    constructor(props) {
@@ -13,6 +14,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Body />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const text = {
+  paddingTop: 5,
+}
+
+const Footer = (props) => (
+  <div style={text}>
+    <center>
+        <a
+          href="https://www.instagram.com/_peach.haus/"
+          target="_blank"
+          rel="noopener noreferrer">
+          Instagram
+        </a>
+        <p></p>
+        <a
+          href="https://twitter.com/PeachHaus"
+          target="_blank"
+          rel="noopener noreferrer">
+          Twitter
+        </a>
+    </center>
+  </div>
+);
+
+export default Footer;
